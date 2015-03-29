0294b2331ee29f73c83cce840548eaacc077c8f9
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-03-29 15:00:43 -0400

    Completed exercise 2

--- Lab09/src/edu/waketech/csc151/lab09/TicketType.java
+++ Lab09/src/edu/waketech/csc151/lab09/TicketType.java
@@ -1,9 +1,18 @@
-
 package edu.waketech.csc151.lab09;
-
 
 public enum TicketType
 {
-	
+	Child(6.0), Adult(9.5), Senior(6.5);
 
+	private final double price;
+
+	TicketType(double id)
+	{
+		this.price = id;
+	}
+
+	public double getPrice()
+	{
+		return price;
+	}
 }
