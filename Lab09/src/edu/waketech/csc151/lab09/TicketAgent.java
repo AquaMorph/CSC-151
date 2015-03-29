09e68522f4c8070e942d672edb3ad90583d5f5fa
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-03-29 15:10:37 -0400

    Completed exercise 3

--- Lab09/src/edu/waketech/csc151/lab09/TicketAgent.java
+++ Lab09/src/edu/waketech/csc151/lab09/TicketAgent.java
@@ -1,7 +1,34 @@
-
 package edu.waketech.csc151.lab09;
 
 public class TicketAgent
 {
+	private String name;
+	private int numberSold = 0;
+	private double sales = 0;
+
+	TicketAgent(String agentName)
+	{
+		name = agentName;
+	}
 	
+	public void sale(TicketType ticket)
+	{
+		numberSold++;
+		sales += ticket.getPrice();
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public double getNumberSold()
+	{
+		return numberSold;
+	}
+
+	public double getSales()
+	{
+		return sales;
+	}
 }
